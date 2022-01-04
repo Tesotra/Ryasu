8bce063f5a7d10e5932e7fbfac3dc2b5f40b2bb1
Texas1000 <microondasconqueso69@gmail.com>
2022-01-03 23:21:22 -0400

    Create Game and FPS Counter

---
+++ Ryasu/Game/Global/UI/FPSCounter.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Extended.BitmapFonts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wobble.Graphics.Sprites;
+
+namespace Ryasu.Game.Global.UI
+{
+    public class FPSCounter : SpriteTextBitmap
+    {
+
+        private TimeSpan elapsedTimeUpdate;
+        private TimeSpan elapsedTimeDraw;
+
+        private int updateCount = 0;
+        private int frameCount = 0;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="font"></param>
+        /// <param name="text"></param>
+        /// <param name="cacheToRenderTarget"></param>
+        public FPSCounter(BitmapFont font, string text, bool cacheToRenderTarget = true) : base(font, text, cacheToRenderTarget)
+        {
+            elapsedTimeUpdate = TimeSpan.Zero;
+            elapsedTimeDraw = TimeSpan.Zero;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            elapsedTimeUpdate += gameTime.ElapsedGameTime;
+
+            if(elapsedTimeUpdate >= TimeSpan.FromSeconds(1))
+            {
+                //Update Text
+                base.Text = $"FPS: {updateCount}\nUPS: {frameCount}";
+
+                //Reset Count
+                updateCount = 0;
+                frameCount = 0;
+
+                //Reset Elapsed Time
+                elapsedTimeUpdate = TimeSpan.Zero;
+                elapsedTimeDraw = TimeSpan.Zero;
+                return;
+            }
+
+            updateCount++;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            frameCount++;
+        }
+    }
+}
---
+++ Ryasu/Game/RyasuGame.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Wobble;
+using Wobble.Window;
+
+namespace Ryasu.Game
+{
+    public class RyasuGame : WobbleGame
+    {
+        public static RyasuGame Instance { get; private set; }
+
+        protected override bool IsReadyToUpdate { get; set; }
+
+        public const string WindowTitle = "Ryasu";
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            Window.Title = WindowTitle;
+
+            Instance = this;
+
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            WindowManager.ChangeScreenResolution(new Point(1280, 720));
+
+            Graphics.SynchronizeWithVerticalRetrace = false;
+            TargetElapsedTime = TimeSpan.FromSeconds(1d / 240);
+            IsFixedTimeStep = true;
+            WaylandVsync = false;
+
+            Window.AllowUserResizing = true;
+
+            Content.RootDirectory = "Content";
+
+            GlobalUserInterface.Cursor.Hide(0);
+
+            IsMouseVisible = true;
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     LoadContent will be called once per game and is the place to load
+        ///     all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            IsReadyToUpdate = true;
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     UnloadContent will be called once per game and is the place to unload
+        ///     game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Allows the game to run logic such as updating the world,
+        ///     checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        protected override void Update(GameTime gameTime)
+        {
+            if (!IsReadyToUpdate)
+                return;
+
+            base.Update(gameTime);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     This is called when the game should draw itself.
+        /// </summary>
+        protected override void Draw(GameTime gameTime)
+        {
+            if (!IsReadyToUpdate)
+                return;
+
+            base.Draw(gameTime);
+        }
+    }
+}
